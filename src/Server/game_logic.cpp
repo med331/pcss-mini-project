02b917ffa82197661056624ad3cfdc326c1171de
steffan christensen <stefc5949@gmail.com>
2019-10-31 10:15:17 +0100

    add empty functions

---
+++ src/Server/game_logic.cpp
@@ -0,0 +1,35 @@
+class game_logic() {
+public :
+	void updateGame(int requestType,int player){
+		//Call functions based to update the game
+	}
+
+private :
+	int cardDeck[];
+	int activePlayer;//probably need to make a player class that can store some info
+
+	void hit() {
+
+	}
+	
+	void stand() {
+
+	}
+
+	void getHand() {
+
+	}
+	
+	void checkHouse() {
+
+	}
+	
+	void drawHouse() {
+
+	}
+
+	void compare() {
+
+	}
+	
+}
