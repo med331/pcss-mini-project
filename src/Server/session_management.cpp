533a28a788df09da687a4ba538c084b3df558c40
Tanja <tanja.andersen@live.dk>
2019-11-05 20:07:09 +0100

    Added classes and functions to session management

--- src/Server/session_management.cpp
+++ src/Server/session_management.cpp
@@ -1,3 +1,18 @@
 //class ConnectionManager(){
 //private GameState currentGamestate=new GameState()
 //}
+class ConnectionManager {
+    private GameState Game;
+
+    public void Unsubscribe (ConnectionThread connectionThread) {}
+    public bool MakeMove (string PlayerID, string Move) {}
+    private void Subscribe (ConnectionThread connectionThread) {}
+    private void UpdateConnections () {}
+    public void SetGame (GameState GameInput) {this.Game=GameInput;}
+    public GameState GetGame () {return this.Game;}
+}
+class ConnectionThread {
+    private string PlayerID;
+    private ConnectionManager Manager;
+    public void EndThread () {}
+}
