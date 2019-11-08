e7df45069511b8686f4ac4fe1616cbad0221ae3c
unaska <uskaal18@student.aau.dk>
2019-11-08 10:32:42 +0100

    server connection test

--- src/Client/server_interface.cpp
+++ src/Client/server_interface.cpp
@@ -1,3 +1,50 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+#define PORT 48820
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+
+    do {
+        send(sock , hello , strlen(hello) , 0 );
+        printf("Hello message sent\n");
+        valread = read( sock , buffer, 1024);
+        printf("%s\n",buffer );
+
+    } while( valread > 0 );
+
+    return 0;
+}
+
 /*
 #include <windows.h>
 #include <winsock2.h>
@@ -5,7 +52,7 @@
 #include <stdlib.h>
 #include <stdio.h>// Receive until the peer closes the connection
 
-int serif main(int argc, char **argv)
+int __cdecl main(int argc, char **argv)
 {
 
 
