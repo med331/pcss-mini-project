93cf0786a50ac1bed3e1967ca332871458ff16c3
unaska <uskaal18@student.aau.dk>
2019-11-11 14:09:23 +0100

    Client/main now compiles!

--- src/Client/main.cpp
+++ src/Client/main.cpp
@@ -1,46 +1,68 @@
+#define WIN32_LEAN_AND_MEAN
+#define _WIN32_WINNT 0x501
+
+#include <windows.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdlib.h>
 #include <stdio.h>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
 
-#define PORT 48820
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT "27015"
 
-int main(int argc, char const *argv[])
+#define DEFAULT_ADDRESS "localhost"
+
+int main()
 {
-    int sock = 0, valread;
-    struct sockaddr_in serv_addr;
-    char *hello = "Hello from client";
-    char buffer[1024] = {0};
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-    {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
+    WSADATA wsaData;
+    SOCKET ConnectSocket = -1;
+    struct addrinfo *result = NULL,
+                    *ptr = NULL,
+                    hints;
+    const char *sendbuf = "Hello, Network!";
+    char recvbuf[DEFAULT_BUFLEN];
+    int iResult;
+    int recvbuflen = DEFAULT_BUFLEN;
+    iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
+    ZeroMemory( &hints, sizeof(hints) );
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
 
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
-    {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
+    iResult = getaddrinfo(DEFAULT_ADDRESS, DEFAULT_PORT, &hints, &result);
 
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
-    {
-        printf("\nConnection Failed \n");
-        return -1;
+    for(ptr=result; ptr != NULL ;ptr=ptr->ai_next) {
+        ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
+            ptr->ai_protocol);
+
+        iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+        if (iResult == -1) {
+            closesocket(ConnectSocket);
+            ConnectSocket = -1;
+        }else{
+            break;
+        }
     }
+    freeaddrinfo(result);
+
+    iResult = send( ConnectSocket, sendbuf, (int)strlen(sendbuf), 0 );
+    printf("Bytes Sent: %d\n", iResult);
+
+    iResult = shutdown(ConnectSocket, 1);
 
     do {
-        send(sock , hello , strlen(hello) , 0 );
-        printf("Hello message sent\n");
-        valread = read( sock , buffer, 1024);
-        printf("%s\n",buffer );
+        iResult = recv(ConnectSocket, recvbuf, recvbuflen, 0);
+        if ( iResult > 0 ){
+            printf("Bytes received: %d\n", iResult);
+            printf("Message received: %s\n", recvbuf);
+        }
+        else if ( iResult == 0 )
+            printf("Connection closed\n");
 
-    } while( valread > 0 );
+    } while( iResult > 0 );
+
+    closesocket(ConnectSocket);
+    WSACleanup();
 
     return 0;
 }
