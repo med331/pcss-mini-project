999ba6eba70509f999128671195f0e67d8bdd445
Peter Leth <16867891+kuff@users.noreply.github.com>
2019-11-22 12:03:22 +0100

    Add comments to server_interface.cpp

--- src/Client/server_interface.cpp
+++ src/Client/server_interface.cpp
@@ -9,16 +9,17 @@
 #include <string>
 #include <iostream>
 
-#define DEFAULT_BUFLEN 512
-#define DEFAULT_PORT "80"
+#define DEFAULT_BUFLEN 512 /* set the maximum packet size */
+#define DEFAULT_PORT "80" /* use port 80, the standard port for HTTP traffic, thereby not requiring any port-forwarding */
 
-#define DEFAULT_ADDRESS "127.0.0.1"
+#define DEFAULT_ADDRESS "127.0.0.1" /* the hard-coded IP address of the test server */
 
 using namespace std;
 
 class ServerInterface
 {
     private:
+        /* variables described during initialization in class constructor */
         WSADATA wsaData;
         SOCKET ConnectSocket = -1;
         struct addrinfo *result = NULL,
@@ -29,40 +30,57 @@ class ServerInterface
         int recvbuflen = DEFAULT_BUFLEN;
 
     public:
+        /**
+         *  Initialize object of type ServerInterface
+         */
         ServerInterface()
         {
             iResult = WSAStartup(MAKEWORD(2,2), &wsaData);
 
-            ZeroMemory( &hints, sizeof(hints) );
-            hints.ai_family = AF_UNSPEC;
-            hints.ai_socktype = SOCK_STREAM;
+            ZeroMemory( &hints, sizeof(hints) );        /* clean out struct */
+            hints.ai_family = AF_UNSPEC;                /* IPv4 or IPv6 does not matter */
+            hints.ai_socktype = SOCK_STREAM;            /* TCP streaming socket */
 
-            iResult = getaddrinfo(DEFAULT_ADDRESS, DEFAULT_PORT, &hints, &result);
+            iResult = getaddrinfo(DEFAULT_ADDRESS,      /* hostname, in this case the remote server IP address */
+                                  DEFAULT_PORT,         /* port 80 */
+                                  &hints, &result);     /* pointer to addrinfo and other relevant information */
         };
 
+        /**
+         *  Attempt to connect to remote server
+         */
         void connectToServer()
         {
+            // establish socket
             ptr=result;
-            ConnectSocket = socket(ptr->ai_family, ptr->ai_socktype,
-                ptr->ai_protocol);
-
-            iResult = connect( ConnectSocket, ptr->ai_addr, (int)ptr->ai_addrlen);
+            ConnectSocket = socket(ptr->ai_family, 
+                                   ptr->ai_socktype, 
+                                   ptr->ai_protocol);
+            // attempt connection
+            iResult = connect(ConnectSocket, 
+                              ptr->ai_addr, 
+                              (int)ptr->ai_addrlen);
+            // give up if connection attempt fails
             if (iResult == -1) {
                 closesocket(ConnectSocket);
                 ConnectSocket = -1;
-
             }
+            // cleanup
             freeaddrinfo(result);
         };
 
+        /**
+         *  Send a list of chars to the server
+         */
         void sendToServer(const char *message)
         {
             iResult = send( ConnectSocket, message, (int)strlen(message), 0 );
             cout << message << endl;
-            /* REMEMBER to add check for message sent */
         };
 
-        /* Actions the client makes, sent to server */
+        /**
+         *  Wrapper function for sendToServer, specifically handling hit or stand
+         */
         void sendAction(bool action)
         {
             if (action) {
@@ -75,14 +93,19 @@ class ServerInterface
             }
         };
 
+        /**
+         *  Receive server event or empty string if no longer connected
+         */
         string recieveFromServer()
         {
             do {
                 recvbuf = new char[recvbuflen];
                 iResult = recv(ConnectSocket, recvbuf, DEFAULT_BUFLEN, 0);
-                if ( iResult > 0 ){
+                // return data if there is any
+                if ( iResult > 0 ) {
                     return recvbuf;
                 }
+                // error handling
                 else if ( iResult == 0 )
                     printf("Connection closed\n");
                 else
@@ -92,6 +115,9 @@ class ServerInterface
             return "";
         };
 
+        /**
+         *  Close server connection and clean up
+         */
         void closeConnection()
         {
             shutdown(ConnectSocket, 1);
