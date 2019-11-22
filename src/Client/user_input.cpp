408715edf553a3378c0bcb82f71714cc33dffbcf
Peter Leth <16867891+kuff@users.noreply.github.com>
2019-11-22 12:04:17 +0100

    Remove unused method "PlayerJoin"

--- src/Client/user_input.cpp
+++ src/Client/user_input.cpp
@@ -2,19 +2,6 @@
 
 using namespace std;
 
-/*bool PlayerJoin (){
-    cout<<"Do you want to join a game??"<<endl;
-    cout<<"[Y]es or [N]o"<<endl;
-    string input;
-    cin>>input;
-
-    if(input=="Y"||input=="y") {
-        return true;
-    } else {
-        return true; /* kekw
-    }
-}*/
-
 // Takes input from player if they want to hit or stand
 bool PlayerHit (){
     cout<<"Do you wish to [H]it or [S]tand?"<<endl;
