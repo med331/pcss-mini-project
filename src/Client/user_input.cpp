e3bc74a87d6031ea3ce603fe4f9362f8e755ba7a
Peter Leth <16867891+kuff@users.noreply.github.com>
2019-11-22 12:05:41 +0100

    Add comments to user_input.cpp

--- src/Client/user_input.cpp
+++ src/Client/user_input.cpp
@@ -2,17 +2,22 @@
 
 using namespace std;
 
-// Takes input from player if they want to hit or stand
+/**
+ *  Ask the player to either hit or stand
+ */
 bool PlayerHit (){
+    /* prompt the user */
     cout<<"Do you wish to [H]it or [S]tand?"<<endl;
+    
+    /* listen for input */
     string input;
     cin>>input;
-
+    
+    /* parse input */
     if(input=="H"||input=="h") {
         return true;
     }
     else {
         return false;
     }
-
 }
